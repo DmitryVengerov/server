b8aa4090d8c802592cd8f381f79085a64710d9d8
Vengerov Dmitrij <hardbeat34@gmail.com>
2018-01-26 00:17:36 +0300

    some test request to couchdb

--- index.js
+++ index.js
@@ -5,13 +5,39 @@ const fs = require('fs');
 const multer = require('multer');
 
 
+
+var request = require('request')
+
+var url = 'http://127.0.0.1:5984/'
+var db = 'todolist/'
+var id = 'document_id'
+
+// Create a database/collection inside CouchDB
+request.put(url + db, function(err, resp, body) {
+    // Add a document with an ID
+    request.put({
+        url: url + db + id,
+        body: { message: 'New Shiny Document', user: 'stefan' },
+        json: true,
+    }, function(err, resp, body) {
+        // Read the document
+        request(url + db + id, function(err, res, body) {
+            console.log(typeof(body))
+            console.log(body)
+            console.log(body.user + ' : ' + body.message)
+        })
+    })
+})
+
+
+
 // this saves your file into a directory called "doc"
 const storage = multer.diskStorage({
-  destination: 'doc/',
-  filename: (req, file, cb) => {
-    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-}); 
+    destination: 'doc/',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
 
 // check list for upload to server 
 const upload = multer({
@@ -26,7 +52,7 @@ const upload = multer({
 }).single('file-pdf');
 
 // little one for check types of files
-function checkFileType(file, cb){
+function checkFileType(file, cb) {
     // Allowed ext
     const filetypes = /jpg|jpeg|png|gif/;
     // Check ext
@@ -34,8 +60,8 @@ function checkFileType(file, cb){
     // Check mime 
     const mimetype = filetypes.test(file.mimetype);
 
-    if(mimetype && extname){
-        return cb(null,true);
+    if (mimetype && extname) {
+        return cb(null, true);
     } else {
         cb('Error: Images only')
     }
@@ -58,27 +84,27 @@ app.get('/docs', (req, res) => {
     // for example we give you simple.pdf. I know its bad
     var filePath = "/doc/simple.pdf";
 
-    fs.readFile(__dirname + filePath , (err,data) => {
+    fs.readFile(__dirname + filePath, (err, data) => {
         res.contentType("application/pdf");
         res.send(data);
     });
 });
 
 // display test form
-app.get('/upload', (req,res) => {
-        res.sendFile(path.join(__dirname + '/public/form.html'));
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/form.html'));
 });
 
 // save file 
-app.post('/upload',(req, res) => {    
-    upload(req,res,(err) => {
-        if(err){
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
             console.log('err')
         } else {
-            if(req.file == undefined){
+            if (req.file == undefined) {
                 console.log('No file selected')
             } else {
-                console.log('file upload to doc/'+req.file.filename);
+                console.log('file upload to doc/' + req.file.filename);
             }
         }
     })
@@ -86,5 +112,5 @@ app.post('/upload',(req, res) => {
 
 // connect to port 3000
 app.listen(3000, () => {
-  console.log('Server start');
+    console.log('Server start');
 });
